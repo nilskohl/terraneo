5722bf89e64c42f2cfbfb68212f6b9667ada8892
Nils Kohl <nils.kohl@lmu.de>
2025-07-31 12:08:09 +0200

    Simple 2 procs MPI test.

---
+++ tests/test_mpi_2_proc.cpp
@@ -0,0 +1,61 @@
+
+
+#include <iostream>
+#include <mpi.h>
+
+#include "terra/kokkos/kokkos_wrapper.hpp"
+#include "util/init.hpp"
+
+int main( int argc, char** argv )
+{
+    terra::util::TerraScopeGuard terra_scope_guard( &argc, &argv );
+
+    const auto rank          = terra::mpi::rank();
+    const auto num_processes = terra::mpi::num_processes();
+
+    if ( num_processes != 2 )
+    {
+        throw std::logic_error( "This test requires 2 MPI processes." );
+    }
+
+    std::cout << "Hello from rank " << rank << " out of " << num_processes << " processes\n";
+
+    Kokkos::View< double* > src( "src", 10 );
+    Kokkos::View< double* > dst( "dst", 10 );
+
+    // Init src
+    if ( rank == 0 )
+    {
+        Kokkos::parallel_for(
+            "fill_src", Kokkos::RangePolicy( 0, 10 ), KOKKOS_LAMBDA( const int i ) {
+                src( i ) = static_cast< double >( i );
+            } );
+
+        Kokkos::fence();
+
+        std::cout << "Sending View from rank " << rank << " to rank 1.\n";
+
+        MPI_Send( src.data(), src.span(), MPI_DOUBLE, 1, 0, MPI_COMM_WORLD );
+    }
+
+    if ( rank == 1 )
+    {
+        MPI_Recv( dst.data(), dst.span(), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+
+        std::cout << "Received View from rank 0 at rank " << rank << ".\n";
+
+        auto dst_host = Kokkos::create_mirror_view( dst );
+        Kokkos::deep_copy( dst_host, dst );
+
+        for ( int i = 0; i < dst_host.extent( 0 ); i++ )
+        {
+            if ( ( dst_host( i ) - i ) > 1e-16 )
+            {
+                throw std::logic_error( "Communication did not succeed." );
+            }
+            std::cout << dst_host( i ) << std::endl;
+        }
+    }
+
+    return 0;
+}
