edf403fe2d8511a65a88b4dce8c81967d1e2152c
Nils Kohl <nils.kohl@lmu.de>
2025-09-12 15:15:14 +0200

    Pushing additional quadrature function that writes values into memory using a ptr arg.

--- src/terra/fe/wedge/quadrature/quadrature.hpp
+++ src/terra/fe/wedge/quadrature/quadrature.hpp
@@ -60,8 +60,30 @@ KOKKOS_INLINE_FUNCTION constexpr void
 }
 
 template < std::floating_point T >
+KOKKOS_INLINE_FUNCTION constexpr void quad_felippa_3x2_quad_points_ptr( dense::Vec< T, 3 >* quad_points )
+{
+    quad_points[0] = { 0.6666666666666666, 0.1666666666666667, -0.5773502691896257 };
+    quad_points[1] = { 0.1666666666666667, 0.6666666666666666, -0.5773502691896257 };
+    quad_points[2] = { 0.1666666666666667, 0.1666666666666667, -0.5773502691896257 };
+    quad_points[3] = { 0.6666666666666666, 0.1666666666666667, 0.5773502691896257 };
+    quad_points[4] = { 0.1666666666666667, 0.6666666666666666, 0.5773502691896257 };
+    quad_points[5] = { 0.1666666666666667, 0.1666666666666667, 0.5773502691896257 };
+}
+
+template < std::floating_point T >
 KOKKOS_INLINE_FUNCTION constexpr void
     quad_felippa_3x2_quad_weights( T ( &quad_weights )[quad_felippa_3x2_num_quad_points] )
+{
+    quad_weights[0] = 0.1666666666666667;
+    quad_weights[1] = 0.1666666666666667;
+    quad_weights[2] = 0.1666666666666667;
+    quad_weights[3] = 0.1666666666666667;
+    quad_weights[4] = 0.1666666666666667;
+    quad_weights[5] = 0.1666666666666667;
+}
+
+template < std::floating_point T >
+KOKKOS_INLINE_FUNCTION constexpr void quad_felippa_3x2_quad_weights_ptr( T* quad_weights )
 {
     quad_weights[0] = 0.1666666666666667;
     quad_weights[1] = 0.1666666666666667;
